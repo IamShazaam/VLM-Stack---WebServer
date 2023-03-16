05275fc41ad9f58cc1e41fdcc014027ccf6ec020
Jorge Pereda <jorge.pereda@chasacademy.se>
2023-03-16 23:18:08 +0100

    db successfully conected

--- backEnd/routes/api.php
+++ backEnd/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DatabaseTestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//endPoint to check a GET response in POSTMAN
+Route::get('/test-connections', [DatabaseTestController::class, 'testConnection']);
